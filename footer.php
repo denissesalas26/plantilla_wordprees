b5eac1a491e0b9ac73ab6ca3adfc44aefc7c8855
denissesalas26 <vanessasalassaavedra26@gmail.com>
2023-04-19 10:00:51 -0500

    aa

---
+++ footer.php
@@ -0,0 +1,10 @@
+<footer class="bg-dark text-white text-center py-3">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <p>&copy; Denisse Salas Saavedra - © 2023-2024</p>
+        </div>
+      </div>
+    </div>
+  </footer>
+  
---
+++ index.php
@@ -0,0 +1,115 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Denisse Salas Saavedra</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+  </head>
+  
+<body>
+
+<?php include('menu.php'); ?>
+
+
+<div class="container my=3">
+    
+     <h1>Denisse Salas Saavedra</h1> <br>
+    
+     <div class="row">
+
+       <div class="col-lg-4 col-sm-12 col-md-6 md-3">
+         <div class="card">
+           <img src="img/GettyImages-532495100_master.jpeg" alt="Imagen 1" class="card-img-top">
+           <div class="card-body">
+             <h5 class="card-title">vestido</h5>
+             <p class="card-text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Provident perferendis quia id maiores pariatur, expedita tenetur est, beatae, repudiandae numquam fugiat eos tempore fuga at vitae quo eligendi voluptates nihil.</p>
+             <a href="#" class="btn btn-primary">Comprar</a>
+             <a href="#" class="btn btn-primary">Añadir al carrito</a>
+           </div>
+         </div>
+       </div>
+      
+       <div class="col-lg-4 col-sm-12 col-md-6 md-3">
+         <div class="card">
+           <img src="img/GettyImages-532495100_master.jpeg" alt="Imagen 2" class="card-img-top">
+           <div class="card-body">
+             <h5 class="card-title">vestido</h5>
+             <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta nam adipisci sit praesentium sunt obcaecati eos culpa quidem inventore facilis, animi esse deserunt itaque sequi aperiam sed, ab quia dolores!</p>
+             <a href="#" class="btn btn-primary">Comprar</a>
+             <a href="#" class="btn btn-primary">Añadir al carrito</a>
+           </div>
+         </div>
+       </div>
+      
+      
+      
+       <div class="col-lg-4 col-sm-12 col-md-6 md-3">
+         <div class="card">
+           <img src="img/GettyImages-532495100_master.jpeg" alt="Imagen 3" class="card-img-top">
+           <div class="card-body">
+             <h5 class="card-title">vestido</h5>
+             <p class="card-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rem recusandae error est tenetur dolores, pariatur quibusdam perspiciatis quaerat! Quisquam ad accusamus corrupti. Natus, numquam. Magni exercitationem quasi ullam repudiandae architecto.</p>
+             <a href="#" class="btn btn-primary">Comprar</a>
+             <a href="#" class="btn btn-primary">Añadir al carrito</a>
+           </div>
+         </div>
+        </div>
+        
+       
+     
+     
+    
+      <div class="col-lg-4 col-sm-12 col-md-6 md-3">
+         <div class="card">
+           <img src="img/GettyImages-532495100_master.jpeg" alt="Imagen 4" class="card-img-top">
+           <div class="card-body">
+             <h5 class="card-title">vestido</h5>
+             <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque atque ratione totam molestias beatae sequi ullam nostrum quisquam cupiditate ex alias, mollitia magni animi sunt! Quibusdam, assumenda illum. Aspernatur, assumenda.</p>
+             <a href="#" class="btn btn-primary">Comprar</a>
+             <a href="#" class="btn btn-primary">Añadir al carrito</a>
+           </div>
+         </div>
+       </div>
+      
+       
+      
+       <div class="col-lg-4 col-sm-12 col-md-6 md-3">
+         <div class="card">
+           <img src="img/GettyImages-532495100_master.jpeg" alt="Imagen 5" class="card-img-top">
+           <div class="card-body">
+             <h5 class="card-title">vestido</h5>
+             <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, accusamus inventore. Magni error exercitationem, culpa sunt harum recusandae ipsa fugit nulla ipsam esse necessitatibus tenetur atque ut quibusdam quasi reiciendis.</p>
+             <a href="#" class="btn btn-primary">Comprar</a>
+             <a href="#" class="btn btn-primary">Añadir al carrito</a>
+           </div>
+         </div>
+       </div>
+       
+       <div class="col-lg-4 col-sm-12 col-md-6 md-3">
+         <div class="card">
+           <img src="img/GettyImages-532495100_master.jpeg" alt="Imagen 6" class="card-img-top">
+           <div class="card-body">
+             <h5 class="card-title">vestido</h5>
+             <p class="card-text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque blanditiis officiis explicabo reprehenderit dolores nostrum est inventore id unde molestiae dolor, quibusdam ipsum ea modi dolorem repudiandae necessitatibus itaque tenetur.</p>
+             <a href="#" class="btn btn-primary">Comprar</a>
+             <a href="#" class="btn btn-primary">Añadir al carrito</a>
+           </div>
+         </div>
+       </div>
+       </div> 
+       </div><br>
+      
+
+       <?php include('footer.php'); ?>
+ 
+       
+  
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+
+
+
+
+</body>
+
